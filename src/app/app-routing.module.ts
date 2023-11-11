0fa9a9f94fd781020d09bb118645db2d03a1989a
arismamo <arismamo@gmail.com>
2023-11-11 11:02:03 +0100

    - Added navigation to header menu items - Refactored login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,19 @@ import {PaymentComponent} from "./pages/payment/payment.component";
 import {ProductDetailsComponent} from "./pages/product-details/product-details.component";
 import {ProductsComponent} from "./pages/products/products.component";
 import {SignUpComponent} from "./pages/sign-up/sign-up.component";
+import {WishlistComponent} from "./pages/wishlist/wishlist.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home-page', pathMatch: 'full'},
   {path: 'home-page', component:HomepageComponent},
-  {path: 'contact-us', component:AboutUsComponent},
+  {path: 'about-us', component:AboutUsComponent},
   {path: 'cart', component: CartComponent},
+  {path: 'wishlist', component: WishlistComponent},
   {path: 'login', component: LoginComponent},
   {path: 'payment', component: PaymentComponent},
   {path: 'product-details/:id', component: ProductDetailsComponent},
-  {path: 'products', component: ProductsComponent , data:{navigateToList: false}},
+  {path: 'products', component: ProductsComponent , data:{ navigateToList: false}},
+  {path: 'products/:category', component: ProductsComponent , data:{ navigateToList: false}},
   {path: 'sign-up', component: SignUpComponent},
 
 
